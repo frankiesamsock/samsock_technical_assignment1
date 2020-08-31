eed0288e4c10958f4c758c02e354e5d674d1128f
frankiesamsock <61481288+frankiesamsock@users.noreply.github.com>
2020-08-30 20:29:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace samsock_technical_assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
